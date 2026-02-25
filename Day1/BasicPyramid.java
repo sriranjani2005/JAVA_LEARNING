5140e8611b1d3be9c0bf0d30c7d299b0dfcc3ea5
sriranjani2005 <sriranjanivs@gmail.com>
2026-02-25 11:51:14 +0530

    BasicPyramid Program

---
+++ Day1/BasicPyramid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BasicPyramid {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number : ");
+    int a = sc.nextInt();
+    System.out.println("   "+a);
+    System.out.println("  "+a+a+a);
+    System.out.println(" "+a+a+a+a+a);
+    System.out.println(""+a+a+a+a+a+a+a);
+    
+}
+}
